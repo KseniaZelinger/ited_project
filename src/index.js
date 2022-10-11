25f03be62a36afa2af0cdcd132438f1afc98bcda
Haze272 <studiorain27@gmail.com>
2022-10-12 00:39:29 +0300

    Разметка и логика консоли

--- src/index.js
+++ src/index.js
@@ -1,3 +1,79 @@
-import './styles.css';
+import './css/reset.css';
+import './css/global.css';
 
-alert('salam1')
+const history = document.getElementById('history');
+const input = document.getElementById('input');
+const cursor = document.getElementById('cursor');
+
+function focusAndMoveCursorToTheEnd(e) {  
+  input.focus();
+  
+  const range = document.createRange();
+  const selection = window.getSelection();
+  const { childNodes } = input;
+  const lastChildNode = childNodes && childNodes.length - 1;
+  
+  range.selectNodeContents(lastChildNode === -1 ? input : childNodes[lastChildNode]);
+  range.collapse(false);
+
+  selection.removeAllRanges();
+  selection.addRange(range);
+}
+
+function handleCommand(command) {
+  const line = document.createElement('DIV');
+  
+  line.textContent = `> ${ command }`;
+  
+  history.appendChild(line);
+}
+
+document.addEventListener('selectionchange', () => {
+  if (document.activeElement.id !== 'input') return;
+  
+  const range = window.getSelection().getRangeAt(0);
+  const start = range.startOffset;
+  const end = range.endOffset;
+  const length = input.textContent.length;
+  
+  if (end < length) {
+    input.classList.add('noCaret');
+  } else {
+    input.classList.remove('noCaret');
+  }
+});
+
+input.addEventListener('input', () => {
+  if (input.childElementCount > 0) {
+    const lines = input.innerText.replace(/\n$/, '').split('\n');
+    const lastLine = lines[lines.length - 1];
+    
+    for (let i = 0; i <= lines.length - 2; ++i) {
+      handleCommand(lines[i]);
+    }
+  
+    input.textContent = lastLine;
+    
+    focusAndMoveCursorToTheEnd();
+  }
+  
+  if (input.innerText.length === 0) {
+    input.classList.remove('noCaret');  
+  }  
+});
+
+document.addEventListener('keydown', (e) => {
+  if (e.target !== input) focusAndMoveCursorToTheEnd();
+});
+
+input.addEventListener('keydown', (e) => {    
+  if (e.key === 'Enter') {
+    e.preventDefault();
+        
+    handleCommand(input.textContent);    
+    input.textContent = '';
+    focusAndMoveCursorToTheEnd();
+  }
+});
+
+input.focus();
